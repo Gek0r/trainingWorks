a31fdcb7b66d7a5349b453b665a43b131c106296
Gekk0 <nolancraze@gmail.com>
2022-07-20 13:33:47 -0500

    Java assignment for Santosh batch, each class is the prompt given.

---
+++ dayThree/src/main/java/dayThree/com/usk/test/Prompt2.java
@@ -0,0 +1,25 @@
+package dayThree.com.usk.test;
+
+public class Prompt2 {
+
+	public static void main(String[] args) {
+		// Take command like argument and check it is odd or even number using if statement
+		
+		//input var
+		int num = 10;
+		
+		//check if the remainder is 0
+		if(num % 2 == 0)
+		{
+			//if remainder is zero, then it is even
+			System.out.println("The number is even");
+		}
+		else
+		{
+			//otherwise it is odd
+			System.out.println("The number is odd");
+		}
+
+	}
+
+}
---
+++ dayThree/src/main/java/dayThree/com/usk/test/Prompt8.java
@@ -0,0 +1,21 @@
+package dayThree.com.usk.test;
+
+public class Prompt8 {
+
+	public static void main(String[] args) {
+		// print odd numbers between 50 to 100 using do while loop
+		int i = 50;
+		do
+		{
+			//if it has a remainder, print it
+			if(i % 2 != 0)
+			{
+				System.out.println(i);
+			}
+			//add to i
+			i++;
+			//check to see if i is less than max value stil
+		}while(i < 100);
+	}
+
+}
